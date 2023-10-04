a66f36c032976ba52c6d2776ba81ca79852e6a17
Guilherme Nóbrega Gomes Dantas <100214869+GuilhermeNobrega@users.noreply.github.com>
2023-10-04 12:23:52 -0300

    Update Aprendizado.py

--- Aprendizado.py
+++ Aprendizado.py
@@ -11,18 +11,25 @@ salarios_por_area = {
 experiencia_por_area = {
     "Segurança de Rede": {
         "Junior": "0-2 anos de experiência",
+         "Analista de Segurança": "2-4 anos de experiência",
+        "Engenheiro de Segurança": "5+ anos de experiência",
     },
     "Segurança de Sistemas Operacionais": {
         "Junior": "0-2 anos de experiência",
+         "Analista de Sistemas": "0-2 anos de experiência",
+        "Especialista em Linux": "3-5 anos de experiência",
     },
     "Criptografia": {
         "Junior": "0-2 anos de experiência",
     },
     "Pentester": {
-        "Junior": "0-2 anos de experiência em segurança cibernética",
+        "Junior": "0-2 anos de experiência em segurança cibernética (Pentester-junior)",
+        "Sênior":"Requer 3-5 anos de experiência em segurança cibernética.",
     },
     "Computação Forense": {
         "Junior": "0-2 anos de experiência em investigações digitais",
+        "Analista Forense Junior": "0-2 anos de experiência em investigações digitais",
+        "Investigador Digital Sênior": "3-5 anos de experiência em investigações digitais",
     }
 }
 
@@ -47,11 +54,41 @@ class AreasSeguranca:
             print(f"\nVocê escolheu se especializar em {self.area}.")
             experiencia_area = experiencia_por_area.get(self.area, {}).get("Junior")
             print(f"Experiência necessária para {self.area}: {experiencia_area}")
+class ExperienciaCargo(AreasSeguranca):
+    def __init__(self):
+        super().__init__()
+        self.cargo = None
 
-areas_seguranca = AreasSeguranca()
-info_experiencia = areas_seguranca.get_apresentacao_areas()
-print(info_experiencia)
-areas_seguranca.analise_experiencia()
+    def escolher_cargo(self):
+        if self.area is not None:
+            print(f"Você escolheu se especializar em {self.area}.")
+            experiencia_area = experiencia_por_area.get(self.area, {}).get("Junior")
+            print(f"Experiência necessária para {self.area}: {experiencia_area}")
+            response = input(f"Qual cargo de {self.area} você deseja? ")
+            self.cargo = response
+
+    def mostrar_info_cargo(self):
+        if self.cargo is not None:
+            salario = salarios_por_area.get(self.area)
+            print(f"Você escolheu o cargo de {self.cargo} em {self.area}.")
+            print(f"A média salarial para {self.area} é de ${salario}.")
+
+    def verificar_experiencia_usuario(self):
+        if self.area is not None and self.cargo is not None:
+            experiencia_necessaria = experiencia_por_area.get(self.area, {}).get(self.cargo)
+            if experiencia_necessaria:
+                print(f"Experiência necessária para o cargo de {self.cargo} em {self.area}: {experiencia_necessaria}")
+            else:
+                print(f"Não foi encontrada informação de experiência para o cargo de {self.cargo} em {self.area}.")
+
+if __name__ == '__main__':
+    experiencia_cargo = ExperienciaCargo()
+    info_experiencia = experiencia_cargo.get_apresentacao_areas()
+    print(info_experiencia)
+    experiencia_cargo.analise_experiencia()
+    experiencia_cargo.escolher_cargo()
+    experiencia_cargo.mostrar_info_cargo()
+    experiencia_cargo.verificar_experiencia_usuario()
 #-------------------------------- old---------------------------------------------
 from time import sleep
 salarios_por_area = {
